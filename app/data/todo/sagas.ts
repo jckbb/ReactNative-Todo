f9c67958c43a5f2639d8bea0d244fa2b3cad098b
Joseph Burghard <jkbburghard@gmail.com>
2018-10-29 14:27:11 -0400

    add fetch all todo redux

--- app/data/todo/sagas.ts
+++ app/data/todo/sagas.ts
@@ -1,6 +1,6 @@
 import { all, takeEvery, fork, put,  } from 'redux-saga/effects';
 import { TodoActionTypes, Todo } from './types';
-import { createSuccess, createRequest } from './actions';
+import { createSuccess, createRequest, fetchSuccess } from './actions';
 
 function* createWorker(action: ReturnType<typeof createRequest>) {
   const todo: Todo = {
@@ -13,10 +13,18 @@ function* createWorker(action: ReturnType<typeof createRequest>) {
   yield put(createSuccess(todo));
 }
 
-function* todoWatcher() {
+function* fetchWorker() {
+  yield put(fetchSuccess([]));
+}
+
+function* createWatcher() {
   yield takeEvery(TodoActionTypes.CREATE_REQUEST, createWorker);
 }
 
+function* fetchWatcher() {
+  yield takeEvery(TodoActionTypes.FETCH_REQUEST, fetchWorker);
+}
+
 export default function* todoSagas() {
-  yield all([fork(todoWatcher)]);
+  yield all([fork(createWatcher), fork(fetchWatcher)]);
 }
