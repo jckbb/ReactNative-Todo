37171d5d69384986db66430d3bac10d33291449b
jckbb <josephkbburghard@gmail.com>
2018-11-02 12:17:25 -0400

    add create redux

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,8 +1,10 @@
-import { TodoState } from "../data/todo/types";
+import { TodoState } from '../data/todo/types';
+import { CreateState } from '../containers/CreateForm/data/types';
 import { Dispatch, Action, AnyAction } from 'redux';
 
 export interface ApplicationState {
-  todo: TodoState
+  todo: TodoState,
+  create: CreateState
 };
 
 export interface ConnectedReduxProps<A extends Action = AnyAction> {
--- app/store/rootSaga.ts
+++ app/store/rootSaga.ts
@@ -1,6 +1,10 @@
 import { all, fork } from 'redux-saga/effects';
 import todoSagas from '../data/todo/sagas';
+import createSagas from '../containers/CreateForm/data/sagas';
 
 export function* rootSaga() {
-  yield all([fork(todoSagas)]);
+  yield all([
+    fork(todoSagas),
+    fork(createSagas)
+  ]);
 }
