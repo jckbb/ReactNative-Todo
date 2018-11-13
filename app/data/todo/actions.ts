a5770e938d5924cfa14a13a991dd5ea8e0540b1c
jckbb <josephkbburghard@gmail.com>
2018-11-13 13:49:49 -0500

    modify action parameter

--- app/data/todo/actions.ts
+++ app/data/todo/actions.ts
@@ -2,7 +2,7 @@ import { action } from 'typesafe-actions';
 import { TodoActionTypes, Todo } from './types';
 
 export const fetchRequest = () => action(TodoActionTypes.FETCH_REQUEST);
-export const fetchSuccess = (data: Todo[]) => action(TodoActionTypes.FETCH_SUCCESS, data);
+export const fetchSuccess = () => action(TodoActionTypes.FETCH_SUCCESS);
 export const fetchError = (message: string) => action(TodoActionTypes.FETCH_ERROR, message);
 
 export const createRequest = (title: string) => action(TodoActionTypes.CREATE_REQUEST, title);
