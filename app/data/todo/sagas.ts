d26a54ecf87f22662d58714667e91032cd06abeb
jckbb <josephkbburghard@gmail.com>
2018-11-18 12:27:18 -0500

    manage redux api calls and logic

--- app/data/todo/sagas.ts
+++ app/data/todo/sagas.ts
@@ -1,20 +1,42 @@
-import { all, takeEvery, fork, put,  } from 'redux-saga/effects';
+import { all, takeEvery, fork, put } from 'redux-saga/effects';
 import { TodoActionTypes, Todo } from './types';
-import { createSuccess, createRequest, fetchSuccess } from './actions';
+import { updateRequest, updateSuccess, createSuccess, deleteRequest, createRequest, fetchSuccess, deleteSuccess } from './actions';
+import Storage from '../../storage/index';
+import { generateUUID } from '../../utils/uuid';
 
 function* createWorker(action: ReturnType<typeof createRequest>) {
+  const currentTime = Date.now();
   const todo: Todo = {
-    id: '0',
-    timestamp: Date.now(),
+    id: generateUUID(),
+    createdAt: currentTime,
+    updatedAt: currentTime,
     title: action.payload,
-    list: []
+    allTaskIds: []
   };
 
-  yield put(createSuccess(todo));
+  const result = yield Storage.getItem(todo.id);
+  
+  yield put(createSuccess(result));
 }
 
-function* fetchWorker() {
-  yield put(fetchSuccess([]));
+function* fetchAllWorker() {
+  const results = yield Storage.getAllItems();
+
+  for(var result of results) {
+    yield put(fetchSuccess(result));
+  }
+}
+
+function* deleteWorker(action: ReturnType<typeof deleteRequest>) {
+  const result = yield Storage.deleteItem(action.payload);
+
+  yield put(deleteSuccess(result));
+}
+
+function* updateWorker(action: ReturnType<typeof updateRequest>) {
+  const result = yield Storage.setItem(action.payload);
+
+  yield put(updateSuccess(result));
 }
 
 function* createWatcher() {
@@ -22,9 +44,22 @@ function* createWatcher() {
 }
 
 function* fetchWatcher() {
-  yield takeEvery(TodoActionTypes.FETCH_REQUEST, fetchWorker);
+  yield takeEvery(TodoActionTypes.FETCH_REQUEST, fetchAllWorker);
+}
+
+function* deleteWatcher() {
+  yield takeEvery(TodoActionTypes.DELETE_REQUEST, deleteWorker);
+}
+
+function* updateWatcher() {
+  yield takeEvery(TodoActionTypes.UPDATE_REQUEST, updateWorker);
 }
 
 export default function* todoSagas() {
-  yield all([fork(createWatcher), fork(fetchWatcher)]);
+  yield all([
+    fork(createWatcher),
+    fork(fetchWatcher),
+    fork(deleteWatcher),
+    fork(updateWatcher)
+  ]);
 }
