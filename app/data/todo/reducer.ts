9af29445a3a555821c1d7e325745c76e35b124ce
jckbb <josephkbburghard@gmail.com>
2018-11-18 12:25:15 -0500

    reduce todo data

--- app/data/todo/reducer.ts
+++ app/data/todo/reducer.ts
@@ -4,21 +4,44 @@ import { TodoState, TodoActionTypes } from './types';
 const initialState: TodoState = {
   loading: false,
   error: undefined,
-  data: [],
+  byId: {},
+  allIds: []
 }
 
 const reducer: Reducer<TodoState> = (state = initialState, action) => {
   switch(action.type) {
     case TodoActionTypes.CREATE_REQUEST:
+    case TodoActionTypes.FETCH_ALL_REQUEST:
+    case TodoActionTypes.DELETE_REQUEST:
+    case TodoActionTypes.UPDATE_REQUEST:
     case TodoActionTypes.FETCH_REQUEST:
-      return {...state, loading: true}
+      return {...state, loading: true};
     case TodoActionTypes.CREATE_SUCCESS:
-      return {...state, loading: false, data: [...state.data, action.payload]}
-    case TodoActionTypes.CREATE_ERROR:
-    case TodoActionTypes.FETCH_ERROR:
-      return {...state, loading: false, error: action.payload}
     case TodoActionTypes.FETCH_SUCCESS:
-      return {...state, loading: false, data: action.payload}
+      return {
+        ...state,
+        loading: false,
+        allIds: [...state.allIds, action.payload.id],
+        byId: {...state.byId, [action.payload.id]: action.payload}
+      };
+    case TodoActionTypes.DELETE_SUCCESS:
+      const {[action.payload]: {}, ...byIdData} = state.byId;
+      return {
+        ...state,
+        loading:false,
+        allIds: [
+          ...state.allIds.slice(0, state.allIds.indexOf(action.payload)),
+          ...state.allIds.slice(state.allIds.indexOf(action.payload) + 1)
+        ],
+        byId: byIdData
+      };
+      case TodoActionTypes.UPDATE_SUCCESS:
+        const {[action.payload]: {}, ...byIdRemaining} = state.byId;
+        return {
+          ...state,
+          loading: false,
+          byId: {...byIdRemaining, [action.payload.id]: action.payload}
+        }
     default:
       return state;
   }
