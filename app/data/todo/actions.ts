13a09d9f3fad429d3526fe3bd152e345616b3c9b
jckbb <josephkbburghard@gmail.com>
2018-11-18 12:24:37 -0500

    add crud related action creators

--- app/data/todo/actions.ts
+++ app/data/todo/actions.ts
@@ -1,10 +1,22 @@
 import { action } from 'typesafe-actions';
 import { TodoActionTypes, Todo } from './types';
 
-export const fetchRequest = () => action(TodoActionTypes.FETCH_REQUEST);
-export const fetchSuccess = () => action(TodoActionTypes.FETCH_SUCCESS);
+export const fetchRequest = (id: string) => action(TodoActionTypes.FETCH_REQUEST, id);
+export const fetchSuccess = (data: Todo) => action(TodoActionTypes.FETCH_SUCCESS, data);
 export const fetchError = (message: string) => action(TodoActionTypes.FETCH_ERROR, message);
+
+export const fetchAllRequest = () => action(TodoActionTypes.FETCH_ALL_REQUEST);
+export const fetchAllSuccess = (data: Todo) => action(TodoActionTypes.FETCH_ALL_SUCCESS, data);
+export const fetchAllError = (message: string) => action(TodoActionTypes.FETCH_ALL_ERROR, message);
 
 export const createRequest = (title: string) => action(TodoActionTypes.CREATE_REQUEST, title);
 export const createSuccess = (data: Todo) => action(TodoActionTypes.CREATE_SUCCESS, data);
 export const createError = (message: string) => action(TodoActionTypes.CREATE_ERROR, message);
+
+export const deleteRequest = (id: string) => action(TodoActionTypes.DELETE_REQUEST, id);
+export const deleteSuccess = (id: string) => action(TodoActionTypes.DELETE_SUCCESS, id);
+export const deleteErrror = (message: string) => action(TodoActionTypes.DELETE_ERROR, message);
+
+export const updateRequest = (data: Todo) => action(TodoActionTypes.UPDATE_REQUEST, data);
+export const updateSuccess = (data: Todo) => action(TodoActionTypes.UPDATE_SUCCESS, data);
+export const updateError = (message: string) => action(TodoActionTypes.UPDATE_ERROR, message);
