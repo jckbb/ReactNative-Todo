31afde164e5d483de293bc7090272b7ed4db1997
jckbb <josephkbburghard@gmail.com>
2018-11-23 13:53:40 -0500

    link redux to view

--- app/containers/List/components/Item/index.tsx
+++ app/containers/List/components/Item/index.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Todo } from '../../../../data/todo/types';
+import { connect } from 'react-redux';
 import styles from './styles';
+import { makeGetTodoById } from '../../../../data/todo/selectors';
+import { ApplicationState } from '../../../../store';
+import { Todo } from '../../../../data/todo/types';
 
 type State = {};
 type Props = {
-  data: Todo
+  id: string
+};
+type PropsFromDisaptch = {};
+type PropsFromState = {
+  todo: Todo
 };
 
-class Item extends React.Component<Props, State> {
+type AllProps = Props & PropsFromState & PropsFromDisaptch;
+
+class Item extends React.Component<AllProps, State> {
   render() {
-    const { title } = this.props.data;
+    const { title } = this.props.todo;
     return(
       <View style={styles.container} >
-        <Text style={styles.title} >{title}</Text>
+        <Text style={styles.title}>{title}</Text>
       </View>
     );
   }
 }
 
-export default Item;
+const makeMapStateToProps = () => {
+  const getTodoById = makeGetTodoById();
+
+  return (state: ApplicationState, props: Props) => {
+    return {
+      todo: getTodoById(state, props)
+    };
+  }
+}
+
+export default connect(makeMapStateToProps)(Item);
