080856dc1c1c10b027736a27dcbc7dbd2b289e5e
jckbb <josephkbburghard@gmail.com>
2018-11-29 17:17:33 -0500

    remove unused reducer

--- src/common/store/rootReducer.ts
+++ src/common/store/rootReducer.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 import { todoReducer } from '../data/todo/reducer';
-import { createReducer } from '../../screens/Main/CreateForm/data/reducer';
 import { ApplicationState } from '.';
 
 export default combineReducers<ApplicationState>({
   todo: todoReducer,
-  create: createReducer
 });
