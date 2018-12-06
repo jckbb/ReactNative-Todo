5b7d46c7d19dc298305ce121efecd59842e6c340
jckbb <josephkbburghard@gmail.com>
2018-12-06 12:57:38 -0500

    refactor for render prop component

--- src/screens/Main/List/components/Item/index.tsx
+++ src/screens/Main/List/components/Item/index.tsx
@@ -1,40 +1,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
 import styles from './styles';
-import { makeGetTodoById } from 'common/data/todo/selectors';
-import { ApplicationState } from 'common/store';
-import { Todo } from 'common/data/todo/types';
+import Todo from 'common/components/Todo';
 
 type Props = {
   id: string
 };
-type PropsFromDisaptch = {};
-type PropsFromState = {
-  todo: Todo
-};
 
-type AllProps = Props & PropsFromState & PropsFromDisaptch;
-
-class Item extends React.Component<AllProps> {
+class TodoItem extends React.Component<Props> {
   render() {
-    const { title } = this.props.todo;
     return(
-      <View style={styles.container} >
-        <Text style={styles.title}>{title}</Text>
-      </View>
+      <Todo id={this.props.id} >
+        {(injectedProps) => 
+          <View style={styles.container} >
+            <Text >
+              {injectedProps.data.title}
+            </Text>
+          </View>
+        }
+      </Todo>
     );
   }
 }
 
-const makeMapStateToProps = () => {
-  const getTodoById = makeGetTodoById();
-
-  return (state: ApplicationState, props: Props) => {
-    return {
-      todo: getTodoById(state, props)
-    };
-  }
-}
-
-export default connect(makeMapStateToProps)(Item);
+export default TodoItem;
