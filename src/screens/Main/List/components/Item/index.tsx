09c9d3a1b4b7d67574b7f7aa28a3f3046f48b28d
jckbb <josephkbburghard@gmail.com>
2018-12-06 14:56:54 -0500

    add radiobutton component and clean styling

--- src/screens/Main/List/components/Item/index.tsx
+++ src/screens/Main/List/components/Item/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import styles from './styles';
 import Todo from 'common/components/Todo';
+import RadioButton from 'common/components/RadioButton';
 
-type Props = {
+interface Props {
   id: string
 };
 
@@ -11,11 +12,14 @@ class TodoItem extends React.Component<Props> {
   render() {
     return(
       <Todo id={this.props.id} >
-        {(injectedProps) => 
+        {(injectedProp) => 
           <View style={styles.container} >
-            <Text >
-              {injectedProps.data.title}
-            </Text>
+            <RadioButton
+              complete={injectedProp.data.complete} // TODO injectedProp.data.complete
+              handleCompleteChange={injectedProp.handleCompleteChange}
+            >
+              {injectedProp.data.title}
+            </RadioButton>
           </View>
         }
       </Todo>
