a19efb1522dec7b4cc687962e909ffb1bba3b263
jckbb <josephkbburghard@gmail.com>
2018-12-17 18:59:36 -0500

    Add styling to container

--- src/screens/Main/Form/index.tsx
+++ src/screens/Main/Form/index.tsx
@@ -4,6 +4,7 @@ import { reduxForm, Field, InjectedFormProps } from 'redux-form';
 import TextField from './components/TextField';
 import { validate } from 'common/utils/validators';
 import Submit from './components/Submit';
+import styles from './styles';
 
 interface Props {
   onSubmit(values: any): void
@@ -12,7 +13,7 @@ interface Props {
 class Form extends React.Component<InjectedFormProps<{}, Props> & Props> {
   render() {
     return(
-      <View >
+      <View style={styles.formContainer} >
         <Field          
           name={'todoTask'}
           component={TextField} />
