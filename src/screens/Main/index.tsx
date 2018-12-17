f00dec9631062c9b302e645fe5a2b7cdde59477f
jckbb <josephkbburghard@gmail.com>
2018-12-16 21:10:31 -0500

    Add accessibility to store

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,18 +1,33 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import styles from './styles';
-import Create from './Create';
+import Form from './Form';
 import List from './List/index';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { createRequest } from 'common/data/todo/actions';
 
-class Main extends React.Component {
+interface PropsFromDispatch {
+  createTodo: typeof createRequest
+};
+
+class Main extends React.Component<PropsFromDispatch> {
   render() {
     return(
       <View style={styles.container} >
-        <Create />
+        <Form 
+          onSubmit={(values: any) => {
+            this.props.createTodo(values.todoTask);
+          }}
+        />
         <List />
       </View>
     );
   }
 }
 
-export default Main;
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  createTodo: (task: string) => dispatch(createRequest(task))
+});
+
+export default connect(null, mapDispatchToProps)(Main);
