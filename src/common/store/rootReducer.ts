dd6f070c11849621b30d2e9d628f3e36b7553102
jckbb <josephkbburghard@gmail.com>
2018-12-16 21:09:03 -0500

    Add todo form to store

--- src/common/store/rootReducer.ts
+++ src/common/store/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { todoReducer } from '../data/todo/reducer';
 import { ApplicationState } from '.';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers<ApplicationState>({
   todo: todoReducer,
+  form: formReducer
 });
