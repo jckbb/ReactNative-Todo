f715cb372615a9debc0910577f94b8182c579466
jckbb <josephkbburghard@gmail.com>
2018-12-17 19:00:59 -0500

    Handle and display field errors

--- src/screens/Main/Form/components/TextField/index.tsx
+++ src/screens/Main/Form/components/TextField/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View } from 'react-native';
 import { WrappedFieldProps } from 'redux-form';
 import styles from './styles';
 import Error from '../Error';
@@ -11,17 +11,18 @@ interface Props {
 
 class TextField extends React.Component<WrappedFieldProps & Props> {
   render() {
-    const { input, meta: { submitFailed } } = this.props;
+    const { input, meta: { submitFailed, valid } } = this.props;
 
     return(
-      <View>
-        {submitFailed && <Error>{'Required'}</Error>}
+      <View style={styles.fieldContainer} >
         <TextInput
-          style={styles.fieldContainer}
+          style={styles.field}
           onChangeText={input.onChange}
           onBlur={input.onBlur}
           placeholder={'todo task'}
+          value={input.value}
         />
+         {(submitFailed && !valid) && <Error>{'Required'}</Error>}
       </View>
     );
   }
