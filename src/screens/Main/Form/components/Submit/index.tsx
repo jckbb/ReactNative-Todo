09d87fbaad6b59ce87deab171335f893025700e4
jckbb <josephkbburghard@gmail.com>
2018-12-17 19:01:21 -0500

    Add styling

--- src/screens/Main/Form/components/Submit/index.tsx
+++ src/screens/Main/Form/components/Submit/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import { createFormStrings as string } from 'res/strings';
+import styles from './styles';
 
 interface Props {
   onSubmit(): void
@@ -13,8 +14,10 @@ class Submit extends React.Component<Props> {
   
   render() {
     return(
-      <TouchableOpacity onPress={this.handleSubmit.bind(this)} >
-        <Text >
+      <TouchableOpacity
+        style={styles.button}
+        onPress={this.handleSubmit.bind(this)} >
+        <Text style={styles.text} >
           {string.createButtonText}
         </Text>
       </TouchableOpacity>
