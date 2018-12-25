73fda94fb10293a3434b453fb5d6d3e2b2f04597
jckbb <josephkbburghard@gmail.com>
2018-12-25 12:49:40 -0500

    Remove unused method

--- src/common/store/index.ts
+++ src/common/store/index.ts
@@ -1,12 +1,7 @@
 import { TodoState } from '../data/todo/types';
-import { Dispatch, Action, AnyAction } from 'redux';
 import { FormStateMap } from 'redux-form';
 
 export interface ApplicationState {
   todo: TodoState,
   form: FormStateMap
 };
-
-export interface ConnectedReduxProps<A extends Action = AnyAction> {
-  dispatch: Dispatch<A>
-}
