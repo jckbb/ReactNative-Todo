57c922e76d3ccf66600e5d4662ea34d48ff0f7bc
jckbb <josephkbburghard@gmail.com>
2019-02-01 11:17:39 -0500

    Delete unused tests

--- src/__tests__/App.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import App from '../index';
-import * as renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-  const tree = renderer.create(<App />).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
